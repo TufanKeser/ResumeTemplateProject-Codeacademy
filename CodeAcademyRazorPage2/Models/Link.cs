80040eaf18d937520b96b015eb50d78d0d134069
Tufan <tufan.keser@4arctech.com>
2022-10-06 16:27:44 +0300

    Add project files.

---
+++ CodeAcademyRazorPage2/Models/Link.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodeAcademyRazorPage2.Models
+{
+    public class Link
+    {
+        public string Linkedin { get; set; } = "https://www.linkedin.com/in/tufan-keser-853a8813b/";
+        public string Github { get; set; } = "https://github.com/TufanKeser"; 
+
+    }
+}
---
+++ CodeAcademyRazorPage2/Pages/Index.cshtml.cs
@@ -0,0 +1,36 @@
+﻿// Index.cshtml.cs
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CodeAcademyRazorPage2.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace CodeAcademyRazorPage2.Pages
+{
+	public class IndexModel : PageModel
+	{
+		public string FullName { get; set; }
+		public string LinkedInUserName { get; set; }
+		public int YearsOfExperience { get; set; }
+		public List<String> Languages { get; set; }
+        public Link Link_Url{ get; set; }
+        public void OnGet()
+		{
+			FullName = "Tufan Keser";
+			LinkedInUserName = "TufanKeser";
+			Languages = new List<string>
+			{
+				"C#",
+				"Scala",
+				"Python"
+			};
+		}
+
+	}
+
+
+
+}
